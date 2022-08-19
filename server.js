bc0f6660c42e7b58b259978cbf53f1a43e55d197
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-18 17:18:43 -0700

    config

---
+++ server.js
@@ -0,0 +1,15 @@
+//setting modules
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//require routes
+app.use(require('./routes'));
