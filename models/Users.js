1c076d93524dcf0f26e319b09fd46bb94de449f7
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-18 18:46:39 -0700

    user model

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+
+const UsersSchema = new Schema(
+{
+ username: {
+    type: String,
+    required: true,
+    unique: true
+ },
+ email: {
+    type: String,
+    unique: true,
+    required: true,
+ },
+ thoughts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+ }],
+ friends: [{
+    type: Types.ObjectId,
+    ref: "User"
+ }]
+}
+
+)
